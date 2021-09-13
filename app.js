d5cd1c03dfea5a9a0b018539e3825df6690d9284
ewanwang1 <wang95098@gmail.com>
2021-09-13 15:40:41 -0700

    hello world

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const ejs = require("ejs");
 const app = express();
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true })) ;
 
 
 
